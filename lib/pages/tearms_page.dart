c1ff2147c8b5cbc411dac7fa988bb85b50dbb8b4
Krishanthapathum <kgkppremalal@students.nsbm.ac.lk>
2023-08-08 23:13:40 +0530

    Add a init part of tearmsPage

---
+++ lib/pages/tearms_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TearmsPage extends StatefulWidget {
+  const TearmsPage({super.key});
+
+  @override
+  State<TearmsPage> createState() => _TearmsPageState();
+}
+
+class _TearmsPageState extends State<TearmsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
