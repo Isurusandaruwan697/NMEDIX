baae7a304830ecd6fc50b1cd0e1b4b27e9656d6e
mrahalabishek <mrahalabishek@gmail.com>
2023-09-07 22:59:50 +0530

    add instructions

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -77,16 +77,97 @@ class _WelcPageState extends State<WelcPage> {
                           ),
                         ),
                       ),
+          
               Positioned(
-                        left: 15,
-                        top: 352,
-                        child: Container(
-                          width: 255,
-                          height: 250,
-                        ),
+                  left: 15,
+                  top: 352,
+                    child: Align(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width*0.8,
+                        height: 250,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 0,
+                                top: 0,
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width*0.8,
+                                    height: 250,
+                                    decoration: ShapeDecoration(
+                                    color: Colors.white.withOpacity(0.8400000143051147),
+                                    shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
                       ),
-              
-              
+                      shadows: const [
+                        BoxShadow(
+                          color: Color(0xB2B96CFF),
+                          blurRadius: 4,
+                          offset: Offset(0, 0),
+                          spreadRadius: 4,
+                        )
+                      ],
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    left: 10,
+                                    top: 35,
+                                    child: Container(
+                                      width: 235,
+                                      height: 165,
+                                      child: const Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          top: 75,
+                          child: Text(
+                            'Step 2: Schedule  appointments with just a \nfew taps. our health, your way.',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 0,
+                          top: 0,
+                          child: SizedBox(
+                            width: 259,
+                            child: Text(
+                              "Step 1: Log  in to  your account  using  your username and set a password. It's your key to better care!",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 0,
+                          top: 135,
+                          child: Text(
+                            "Step 3: Discover our innovative features for\neffortless  appointment booking and more...",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                    ),
+        ),
 
               
 
