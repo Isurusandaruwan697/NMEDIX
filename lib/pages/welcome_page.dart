c13975579f7da102925e2f97babd46110b099535
mrahalabishek <mrahalabishek@gmail.com>
2023-09-07 21:57:05 +0530

    add img doctor

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -45,6 +45,51 @@ class _WelcPageState extends State<WelcPage> {
                 ),
               ),
 
+              const Positioned(
+                left: 28,
+                top: 200,
+                child: SizedBox(
+                  width: 250,
+                  height: 70,
+                  child: Text(
+                    "Let's get you started on your health journey.\nFollow these steps to unlock a world \nof seamless healthcare:",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+
+              Positioned(
+                        left: 0,
+                        bottom: 0,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width*0.7,
+                          height: MediaQuery.of(context).size.height*0.65,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("doctor.png"),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+              Positioned(
+                        left: 15,
+                        top: 352,
+                        child: Container(
+                          width: 255,
+                          height: 250,
+                        ),
+                      ),
+              
+              
+
+              
+
               ],
             ),
             
