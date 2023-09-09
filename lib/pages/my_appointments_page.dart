817fbc813c0c3fafbe791160e11db00857800268
mrahalabishek <mrahalabishek@gmail.com>
2023-09-10 04:49:00 +0530

    modified box

--- lib/pages/my_appointments_page.dart
+++ lib/pages/my_appointments_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medix/pages/start_page.dart';
 
 
 class MyAppoint extends StatefulWidget {
@@ -69,7 +70,7 @@ class _MyAppointState extends State<MyAppoint> {
 
               Positioned(
                 left: width * 0.10,
-                top: 353,
+                top: height * 0.50,
                 child: Container(
                   width: width * 0.8,
                   height: height * 0.12,
@@ -82,13 +83,126 @@ class _MyAppointState extends State<MyAppoint> {
                           width: width * 0.8,
                           height: height * 0.12,
                           decoration: ShapeDecoration(
-                            color: Colors.black,
+                            color: Colors.white,
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15)
-                            )
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            shadows:  const [
+                              BoxShadow(
+                                color: Color(0x3F000000),
+                                blurRadius: 4,
+                                offset: Offset(0, 0),
+                                spreadRadius: 2,
+                              )
+                            ],
+                            
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        child: Container(
+                          width: width * 0.8,
+                          height: 38,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                  width: width * 0.8,
+                                  height: 38,
+                                  decoration: const ShapeDecoration(
+                                    color: Color(0xFF7C32C1),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(15),
+                                        topRight: Radius.circular(15),
+                                      ),
+                                    ),
+                                    shadows: [
+                                      BoxShadow(
+                                        color: Color(0x3F000000),
+                                        blurRadius: 4,
+                                        offset: Offset(0, 1),
+                                        spreadRadius: 0,
+                                      )
+                                    ]
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: 22,
+                                top: 9,
+                                child: Container(
+                                  width: 289,
+                                  height: 20,
+                                  child:  Stack(
+                                    children: [
+                                      const Positioned(
+                                        left: 0,
+                                        top: 0,
+                                        child: Text(
+                                          'Medical Request',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontFamily: 'Montserrat',
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        left: width * 0.55,
+                                        top: 0,
+                                        child: Container(
+                                          width: width * 0.14,
+                                          height: height *0.03,
+                                          child: Stack(
+                                            children: [
+                                              Positioned(
+                                                left: 0,
+                                                top: 0,
+                                                child: Container(
+                                                  width: width * 0.14,
+                                                  height: height *0.03,
+                                                  decoration: ShapeDecoration(
+                                                    color: Colors.white,
+                                                    shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(15),
+
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                              const Positioned(
+                                                left: 10,
+                                                top: 4,
+                                                child: Text(
+                                                  '07/10',
+                                                  style: TextStyle(
+                                                    color: Color(0xFF7A30C0),
+                                                    fontSize: 10,
+                                                    fontFamily: 'Montserrat',
+                                                    fontWeight: FontWeight.w700,
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      )
+                                    ],
+                                  ),
+
+                                ),
+                              )
+                            ],
                           ),
                         ),
                       )
+
                     ],
                   ),
                 ),
