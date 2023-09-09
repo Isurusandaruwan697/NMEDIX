03f17c44ac13bbafa8e8eefaf78a9897f6fce3e9
mrahalabishek <mrahalabishek@gmail.com>
2023-09-09 16:59:42 +0530

    add my appointments page

---
+++ lib/pages/my_appointments_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+
+class MyAppoint extends StatefulWidget {
+  const MyAppoint({super.key});
+
+  @override
+  State<MyAppoint> createState() => _MyAppointState();
+}
+
+class _MyAppointState extends State<MyAppoint> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child:  Stack(
+              children: [
+                Positioned(
+                left: 0,
+                top: -17,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 205,
+                  decoration: const ShapeDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment(0.00, -1.00),
+                      end: Alignment(0, 1),
+                      colors: [Color(0xFF742BBA), Color(0xFFB96CFF)],
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(24),
+                        bottomRight: Radius.circular(24),
+                      ),
+                    ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(2, 3),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+                const Positioned(
+                left: 28,
+                top: 77,
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    'My \nAppointments',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
