50511fe560fe8888876b9d5b9e2c80a751027032
mrahalabishek <mrahalabishek@gmail.com>
2023-09-10 14:09:27 +0530

    update welcome page recreate

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -22,14 +22,47 @@ class _WelcPageState extends State<WelcPage> {
               children: [
                 Positioned(
                   left: width *0.15,
-                  top: 50,
+                  top: height *0.08,
                   child: Align(
                     alignment: Alignment.topCenter,
                     child: Image(image:const AssetImage('logocolor.png'),
                     fit: BoxFit.cover,
                     width: MediaQuery.of(context).size.width*0.7,),
                   ),
-                )
+                ),
+
+                Positioned(
+                  left: width * 0.08,
+                  top: height *0.25,
+                    child: const Text(
+                    'Welcome to NMEDIX !',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFF742BBA),
+                      fontSize: 18,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w600,
+                    ),
+                                  ),
+                ),
+
+                Positioned(
+                left: width *0.08,
+                top: height *0.3,
+                child: SizedBox(
+                  width: 250,
+                  height: 70,
+                  child: Text(
+                    "Let's get you started on your health journey.\nFollow these steps to unlock a world \nof seamless healthcare:",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
               ],
             ),
           ),
