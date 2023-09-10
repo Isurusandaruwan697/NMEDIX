cbaa7c24f743aa865a7673c4ddc8ac2c49f29089
mrahalabishek <mrahalabishek@gmail.com>
2023-09-10 06:45:16 +0530

    add 2 cards

--- lib/pages/my_appointments_page.dart
+++ lib/pages/my_appointments_page.dart
@@ -72,7 +72,7 @@ class _MyAppointState extends State<MyAppoint> {
                 top: height * 0.50,
                 child: Container(
                   width: width * 0.8,
-                  height: height * 0.12,
+                  height: height * 0.14,
                   child: Stack(
                     children: [
                       Positioned(
@@ -80,7 +80,7 @@ class _MyAppointState extends State<MyAppoint> {
                         top: 0,
                         child: Container(
                           width: width * 0.8,
-                          height: height * 0.12,
+                          height: height * 0.14,
                           decoration: ShapeDecoration(
                             color: Colors.white,
                             shape: RoundedRectangleBorder(
@@ -165,7 +165,7 @@ class _MyAppointState extends State<MyAppoint> {
                                                 top: 0,
                                                 child: Container(
                                                   width: width * 0.14,
-                                                  height: height *0.03,
+                                                  height: height *0.025,
                                                   decoration: ShapeDecoration(
                                                     color: Colors.white,
                                                     shape: RoundedRectangleBorder(
@@ -176,8 +176,8 @@ class _MyAppointState extends State<MyAppoint> {
                                                 ),
                                               ),
                                               const Positioned(
-                                                left: 10,
-                                                top: 4,
+                                                left: 12,
+                                                top: 3.5,
                                                 child: Text(
                                                   '07/10',
                                                   style: TextStyle(
@@ -200,7 +200,225 @@ class _MyAppointState extends State<MyAppoint> {
                             ],
                           ),
                         ),
-                      )
+                      ),
+                      const Positioned(
+                        left: 22,
+                        top: 47,
+                        child: Text(
+                          'Time: 09:00 AM',
+                          style: TextStyle(
+                            color: Color(0xFF333333),
+                            fontSize: 12,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        left: 22,
+                        top: 68,
+                        child: Text(
+                          'Number: ',
+                          style: TextStyle(
+                            color: Color(0xFF333333),
+                            fontSize: 12,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        left: 80,
+                        top: 69,
+                        child: Text(
+                          '15 ',
+                          style: TextStyle(
+                            color: Color(0xFF333333),
+                            fontSize: 12,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: width * 0.10,
+                top: height * 0.32,
+                child: Container(
+                  width: width * 0.8,
+                  height: height * 0.14,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        child: Container(
+                          width: width * 0.8,
+                          height: height * 0.14,
+                          decoration: ShapeDecoration(
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            shadows:  const [
+                              BoxShadow(
+                                color: Color(0x3F000000),
+                                blurRadius: 4,
+                                offset: Offset(0, 0),
+                                spreadRadius: 2,
+                              )
+                            ],
+                            
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        child: Container(
+                          width: width * 0.8,
+                          height: 38,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                  width: width * 0.8,
+                                  height: 38,
+                                  decoration: const ShapeDecoration(
+                                    color: Color(0xFF7C32C1),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(15),
+                                        topRight: Radius.circular(15),
+                                      ),
+                                    ),
+                                    shadows: [
+                                      BoxShadow(
+                                        color: Color(0x3F000000),
+                                        blurRadius: 4,
+                                        offset: Offset(0, 1),
+                                        spreadRadius: 0,
+                                      )
+                                    ]
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: 22,
+                                top: 9,
+                                child: Container(
+                                  width: 289,
+                                  height: 20,
+                                  child:  Stack(
+                                    children: [
+                                      const Positioned(
+                                        left: 0,
+                                        top: 0,
+                                        child: Text(
+                                          'Medical Request',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontFamily: 'Montserrat',
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        left: width * 0.55,
+                                        top: 0,
+                                        child: Container(
+                                          width: width * 0.14,
+                                          height: height *0.03,
+                                          child: Stack(
+                                            children: [
+                                              Positioned(
+                                                left: 0,
+                                                top: 0,
+                                                child: Container(
+                                                  width: width * 0.14,
+                                                  height: height *0.025,
+                                                  decoration: ShapeDecoration(
+                                                    color: Colors.white,
+                                                    shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(15),
+
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                              const Positioned(
+                                                left: 12,
+                                                top: 3.5,
+                                                child: Text(
+                                                  '07/10',
+                                                  style: TextStyle(
+                                                    color: Color(0xFF7A30C0),
+                                                    fontSize: 10,
+                                                    fontFamily: 'Montserrat',
+                                                    fontWeight: FontWeight.w700,
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      )
+                                    ],
+                                  ),
+
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        left: 22,
+                        top: 47,
+                        child: Text(
+                          'Time: 09:00 AM',
+                          style: TextStyle(
+                            color: Color(0xFF333333),
+                            fontSize: 12,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        left: 22,
+                        top: 68,
+                        child: Text(
+                          'Number: ',
+                          style: TextStyle(
+                            color: Color(0xFF333333),
+                            fontSize: 12,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        left: 80,
+                        top: 69,
+                        child: Text(
+                          '15 ',
+                          style: TextStyle(
+                            color: Color(0xFF333333),
+                            fontSize: 12,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
 
                     ],
                   ),
