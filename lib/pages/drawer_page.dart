a65d080cff9e07d7e3000c18c138d7bb54b9b377
Deshaninayanamini <kldnayanamini@students.nsbm.ac.lk>
2023-09-17 15:49:25 +0530

    create drawer page

---
+++ lib/pages/drawer_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Drower extends StatelessWidget {
+  const Drower({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
