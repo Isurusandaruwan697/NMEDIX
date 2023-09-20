e6b0ea1a7dafdfab14a1b758dee1b35705a11c6d
Krishanthapathum <kgkppremalal@students.nsbm.ac.lk>
2023-09-20 12:11:50 +0530

    modify changes

--- lib/pages/drawer_page.dart
+++ lib/pages/drawer_page.dart
@@ -4,6 +4,7 @@ import 'package:medix/pages/article_page.dart';
 import 'package:medix/pages/dashboard_page.dart';
 import 'package:medix/pages/ec_approval_page.dart';
 import 'package:medix/pages/medicalappro_screen.dart';
+import 'package:medix/pages/my_appointments_page.dart';
 import 'package:medix/pages/userprofile.dart';
 
 class Drower extends StatelessWidget {
@@ -19,17 +20,21 @@ class Drower extends StatelessWidget {
         children: [
           UserAccountsDrawerHeader(
             accountName: Text(user.email.toString()),
-            accountEmail: Text("Faulty"),
+            accountEmail: const Text("Faulty"),
             currentAccountPicture: CircleAvatar(
               child: Image.asset('assets/feedback.png'),
             ),
             decoration: const BoxDecoration(
               color: Color(0xFFB96CFF),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(24),
+                bottomRight: Radius.circular(24),
+              ),
             ),
           ),
           ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Profile"),
+            leading: const Icon(Icons.person),
+            title: const Text("Profile"),
             onTap: () {
               Navigator.push(
                 context,
@@ -41,8 +46,8 @@ class Drower extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(Icons.home),
-            title: Text("Dashboard"),
+            leading: const Icon(Icons.home),
+            title: const Text("Dashboard"),
             onTap: () {
               Navigator.push(
                 context,
@@ -51,8 +56,8 @@ class Drower extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(Icons.approval),
-            title: Text("EC Approval"),
+            leading: const Icon(Icons.approval),
+            title: const Text("EC Approval"),
             onTap: () {
               Navigator.push(
                 context,
@@ -61,8 +66,8 @@ class Drower extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(Icons.medical_information),
-            title: Text("Medical Approval"),
+            leading: const Icon(Icons.medical_information),
+            title: const Text("Medical Approval"),
             onTap: () {
               Navigator.push(
                 context,
@@ -72,14 +77,91 @@ class Drower extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(Icons.article),
-            title: Text("Artical"),
+            leading: const Icon(Icons.article),
+            title: const Text("Artical"),
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Article()),
+                MaterialPageRoute(builder: (context) => const Article()),
               );
             },
+          ),
+          ListTile(
+            leading: const Icon(Icons.bookmark_add_outlined),
+            title: const Text("Appoinments"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MyAppoint()),
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  /*onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Dashboard()), 
+                        );
+                      }, */
+                  child: SizedBox(
+                    width: 160,
+                    height: 48,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          top: 0,
+                          child: Container(
+                            width: 160,
+                            height: 48,
+                            decoration: ShapeDecoration(
+                              gradient: const LinearGradient(
+                                begin: Alignment(0.00, -1.00),
+                                end: Alignment(0, 1),
+                                colors: [Color(0xFF742BBA), Color(0xFFB96CFF)],
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              shadows: const [
+                                BoxShadow(
+                                  color: Color(0x54000000),
+                                  blurRadius: 13,
+                                  offset: Offset(0, 0),
+                                  spreadRadius: 2,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        const Positioned(
+                            left: 14,
+                            top: 13,
+                            child: Icon(Icons.logout, color: Colors.white)),
+                        const Positioned(
+                          left: 50,
+                          top: 14,
+                          child: Text(
+                            'Logout',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
